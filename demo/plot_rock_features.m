58006c963ce48ddb5e2c6475b7c51d70af6f787f
DiegoBueso <50967794+DiegoBueso@users.noreply.github.com>
2019-05-24 02:09:49 +0200

    Add files via upload

---
+++ demo/plot_rock_features.m
@@ -0,0 +1,24 @@
+function []=plot_rock_features(Xpc,pc,expv)
+
+h=size(expv,1);
+expv=expv*100/sum(expv);
+
+%% Spatial and Temporal modes plot
+map=cmocean('balance');
+figure,
+for i=1:h
+    subplot(h,2,2*i),
+    plot(real(pc(:,i)),'-r','linewidth',.5);
+    title(['Time Feature $\#',num2str(i),'(',num2str(expv(i)),'\%)$'],'interpreter','latex');
+    grid;
+
+    ax1=subplot(h,2,2*i-1);
+    xr=real(squeeze(Xpc(i,:,:)));
+    contourf(xr);
+    set(gca,'xtick',[]);
+    set(gca,'ytick',[]);
+    grid;title(['Spatial Feature $\#',num2str(i),'(',num2str(expv(i)),'\%)$'],'interpreter','latex');
+    colormap(ax1,map);colorbar;
+    
+end
+end
